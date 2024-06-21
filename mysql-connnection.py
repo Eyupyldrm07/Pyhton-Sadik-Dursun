adc9258c4368bbc48c78a85ec584ca0e4f1bd6a1
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2024-06-21 23:07:41 +0300

    Create mysql-connnection.py

---
+++ mysql-connnection.py
@@ -0,0 +1,11 @@
+# import mysql.connector
+import numbers
+print(numbers.Number)
+
+# mydb = mysql.connector.connect(
+
+#     host ='localhost', #192.23.45.56,
+#     user = 'root',
+#     password = '1927'
+# )
+# print(mydb)
